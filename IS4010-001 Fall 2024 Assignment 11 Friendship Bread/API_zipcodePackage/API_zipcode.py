bbb2c3453c2513711ba38967fabeab4e249ef5db
Ryan Dew <dewrm@mail.uc.edu>
2024-11-14 08:18:52 -0500

    Added API key and documentation.

--- IS4010-001 Fall 2024 Assignment 11 Friendship Bread/API_zipcodePackage/API_zipcode.py
+++ IS4010-001 Fall 2024 Assignment 11 Friendship Bread/API_zipcodePackage/API_zipcode.py
@@ -9,7 +9,18 @@
 # Brief Description of the assignment: This is a team assignment that we are told to collaborate on a python project, we chose to create a sports team matchup program. We have 3 classes 1 for team wins and losses, 1 for total points score and 1 change team name.
 
 # Brief Description of what this module does. This module is a class that models a team matchup, including the opponent's name and the location of the game. It provides methods to get and set these attributes, print matchup details, and represent the object as a string.
-# Citations: Used ChatGPT for code structure and comments.
+# Citations: Used ChatGPT for code structure and comments. https://app.zipcodebase.com/documentation
 # Anything else that's relevant: None
 
 
+import requests
+
+headers = { 
+  "apikey": "aa80fb00-a28a-11ef-8fc3-573b5fa4625f"}
+
+params = (
+   ("codes","10005,51503"),
+);
+
+response = requests.get('https://app.zipcodebase.com/api/v1/search', headers=headers, params=params);
+print(response.text)
